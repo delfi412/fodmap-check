d93aa3434ea5ac35e45a6e22f83cb1a4d77cd9c3
delfi412 <delphine.gendreau@gmail.com>
2026-08-16 16:12:12 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,29 +1,881 @@
 const foods = {
 
-    mela: {
-        color: "orange",
-        portion: "Massimo: 20 g"
+    // =========================
+    // FRUTTA
+    // =========================
+
+    "mele": {
+        color: "red"
     },
 
-    carota: {
+    "albicocche": {
+        color: "red"
+    },
+
+    "more": {
+        color: "red"
+    },
+
+    "ciliegie": {
+        color: "red"
+    },
+
+    "datteri": {
+        color: "red"
+    },
+
+    "fichi": {
+        color: "red"
+    },
+
+    "mango": {
+        color: "red"
+    },
+
+    "pesche noci": {
+        color: "red"
+    },
+
+    "pesche": {
+        color: "red"
+    },
+
+    "pere": {
+        color: "red"
+    },
+
+    "prugne": {
+        color: "red"
+    },
+
+    "prugne secche": {
+        color: "red"
+    },
+
+    "uvetta": {
+        color: "green"
+    },
+
+    "anguria": {
+        color: "red"
+    },
+
+    "banana": {
+        color: "green"
+    },
+
+    "mirtilli": {
+        color: "green"
+    },
+
+    "melone": {
+        color: "green"
+    },
+
+    "clementine": {
+        color: "green"
+    },
+
+    "mirtilli rossi": {
+        color: "green"
+    },
+
+    "melone bianco": {
+        color: "green"
+    },
+
+    "uva": {
+        color: "green"
+    },
+
+    "kiwi": {
+        color: "green"
+    },
+
+    "limone": {
+        color: "green"
+    },
+
+    "lime": {
+        color: "green"
+    },
+
+    "litchi": {
         color: "green",
-        portion: ""
+        portion: "max 5"
     },
 
-    cipolla: {
+    "mandarini": {
+        color: "green"
+    },
+
+    "arance": {
+        color: "green"
+    },
+
+    "frutto della passione": {
+        color: "green"
+    },
+
+    "papaya": {
+        color: "green"
+    },
+
+    "ananas": {
+        color: "green"
+    },
+
+    "melograno": {
+        color: "green",
+        portion: "max 1/2"
+    },
+
+    "lamponi": {
+        color: "green"
+    },
+
+    "fragole": {
+        color: "green"
+    },
+
+
+    // =========================
+    // VERDURA
+    // =========================
+
+    "carciofi": {
+        color: "red"
+    },
+
+    "avocado": {
         color: "red",
-        portion: ""
+        portion: "OK max 1/4"
     },
 
-    banana: {
-        color: "orange",
-        portion: "Massimo: 100 g"
+    "barbabietola": {
+        color: "red",
+        portion: "OK max 35g"
     },
 
-    fragola: {
+    "broccoli": {
+        color: "red",
+        portion: "OK max 35g"
+    },
+
+    "cavoletti di bruxelles": {
+        color: "red",
+        portion: "OK max 35g"
+    },
+
+    "zucca butternut": {
+        color: "red",
+        portion: "OK max 35g"
+    },
+
+    "cavolfiore": {
+        color: "red"
+    },
+
+    "sedano": {
+        color: "red",
+        portion: "OK max 35g"
+    },
+
+    "finocchio": {
+        color: "red",
+        portion: "OK max 35g"
+    },
+
+    "aglio": {
+        color: "red"
+    },
+
+    "porro": {
+        color: "red"
+    },
+
+    "funghi": {
+        color: "red"
+    },
+
+    "piselli": {
+        color: "red",
+        portion: "0K max 35g"
+    },
+
+    "cavolo verza": {
         color: "green",
-        portion: ""
-    }
+        portion: "Solo piccole quantità"
+    },
+
+    "cipolla": {
+        color: "red"
+    },
+
+    "cipolla rossa": {
+        color: "red"
+    },
+
+    "scalogno": {
+        color: "red"
+    },
+
+    "cipollotto": {
+        color: "red"
+    },
+
+    "patata dolce": {
+        color: "red",
+        portion: "0K max 35g"
+    },
+
+    "mais dolce": {
+        color: "red",
+        portion: "0K max 35g"
+    },
+
+    "ceci": {
+        color: "red"
+    },
+
+    "fave": {
+        color: "red"
+    },
+
+    "fagioli cannellini": {
+        color: "red"
+    },
+
+    "fagioli borlotti": {
+        color: "red"
+    },
+
+    "fagioli rossi": {
+        color: "red"
+    },
+
+    "lenticchie": {
+        color: "red"
+    },
+
+
+    "asparagi": {
+        color: "green"
+    },
+
+    "germogli di soia": {
+        color: "green"
+    },
+
+    "melanzane": {
+        color: "green"
+    },
+
+    "carote": {
+        color: "green"
+    },
+
+    "bietola": {
+        color: "green"
+    },
+
+    "peperoncino": {
+        color: "green"
+    },
+
+    "erba cipollina": {
+        color: "green"
+    },
+
+    "zucchine": {
+        color: "green"
+    },
+
+    "cetrioli": {
+        color: "green"
+    },
+
+    "indivia": {
+        color: "green"
+    },
+
+    "fagiolini": {
+        color: "green"
+    },
+
+    "lattuga": {
+        color: "green"
+    },
+
+    "olive": {
+        color: "green"
+    },
+
+    "olive nere": {
+        color: "green"
+    },
+
+    "olive verdi": {
+        color: "green"
+    },
+
+    "pak choi": {
+        color: "green"
+    },
+
+    "peperoni": {
+        color: "green"
+    },
+
+    "patate": {
+        color: "green"
+    },
+
+    "zucca": {
+        color: "green"
+    },
+
+    "ravanello": {
+        color: "green"
+    },
+
+    "rucola": {
+        color: "green"
+    },
+
+    "alghe": {
+        color: "green"
+    },
+
+    "spinaci": {
+        color: "green"
+    },
+
+    "pomodori": {
+        color: "green"
+    },
+
+    "rape": {
+        color: "green"
+    },
+
+    "cavolo bianco": {
+        color: "green"
+    },
+
+    "cavolo rosso": {
+        color: "green"
+    },
+
+
+    // =========================
+    // CEREALI E AMIDI
+    // =========================
+
+    "grano": {
+        color: "red"
+    },
+
+    "bulgur": {
+        color: "red"
+    },
+
+    "cous cous": {
+        color: "red"
+    },
+
+    "semolino": {
+        color: "red"
+    },
+
+    "segale": {
+        color: "red"
+    },
+
+    "orzo": {
+        color: "red"
+    },
+
+    "riso": {
+        color: "green"
+    },
+
+    "avena": {
+        color: "green"
+    },
+
+    "grano saraceno": {
+        color: "green"
+    },
+
+    "polenta": {
+        color: "green"
+    },
+
+    "mais": {
+        color: "green"
+    },
+
+    "quinoa": {
+        color: "green"
+    },
+
+
+    // =========================
+    // PANE E PASTA
+    // =========================
+
+    "pane di frumento": {
+        color: "red"
+    },
+
+    "pane integrale": {
+        color: "red"
+    },
+
+    "pane multicereali": {
+        color: "red"
+    },
+
+    "pane di segale": {
+        color: "red"
+    },
+
+    "pane di farro": {
+        color: "red"
+    },
+
+    "pane senza glutine": {
+        color: "green"
+    },
+
+    "gnocchi": {
+        color: "red"
+    },
+
+    "pasta di ceci": {
+        color: "red"
+    },
+
+    "pasta di lenticchie": {
+        color: "red"
+    },
+
+    "pasta senza glutine": {
+        color: "green"
+    },
+
+    "pasta di grano saraceno": {
+        color: "green"
+    },
+
+    "spaghetti di riso": {
+        color: "green"
+    },
+
+    "noodles di grano saraceno": {
+        color: "green"
+    },
+
+
+    // =========================
+    // FRUTTA SECCA E SEMI
+    // =========================
+
+    "anacardi": {
+        color: "red"
+    },
+
+    "pistacchi": {
+        color: "red"
+    },
+
+    "cocco essiccato": {
+        color: "green",
+        portion: "OK piccole quantità"
+    },
+
+    "mandorle": {
+        color: "green",
+        portion: "max 10"
+    },
+
+    "nocciole": {
+        color: "green",
+        portion: "max 10"
+    },
+
+    "noci macadamia": {
+        color: "green"
+    },
+
+    "arachidi": {
+        color: "green"
+    },
+
+    "noci pecan": {
+        color: "green"
+    },
+
+    "pinoli": {
+        color: "green"
+    },
+
+    "semi di zucca": {
+        color: "green"
+    },
+
+    "semi di sesamo": {
+        color: "green"
+    },
+
+    "semi di girasole": {
+        color: "green"
+    },
+
+    "noci": {
+        color: "green"
+    },
+
+    "burro di arachidi": {
+        color: "green"
+    },
+
+
+    // =========================
+    // ZUCCHERI E DOLCIFICANTI
+    // =========================
+
+    "miele": {
+        color: "red"
+    },
+
+    "sciroppo d'agave": {
+        color: "red"
+    },
+
+    "zucchero": {
+        color: "green"
+    },
+
+    "sciroppo d'acero": {
+        color: "green"
+    },
+    
+    "marmellata": {
+        color: "green",
+        portion: "di frutti idonei"
+    },
+
+    "caramelle, gomme e mentine senza zucchero": {
+        color: "red"
+    },
+
+
+    // =========================
+    // LATTE E LATTICINI
+    // =========================
+
+    "latte con lattosio": {
+        color: "red"
+    },
+
+    "latte senza lattosio": {
+        color: "green"
+    },
+
+    "latte di mandorla": {
+        color: "green"
+    },
+
+    "latte di nocciola": {
+        color: "green"
+    },
+
+    "latte di soia": {
+        color: "green",
+        portion: "max 60ml"
+    },
+
+    "latte di riso": {
+        color: "green",
+        portion: "max 200ml"
+    },
+
+    "latte d'avena": {
+        color: "green",
+        portion: "max 50ml"
+    },
+
+    "yogurt con lattosio": {
+        color: "red"
+    },
+
+    "yogurt senza lattosio": {
+        color: "green"
+    },
+
+    "yogurt di soia": {
+        color: "green",
+        portion: "max 125 g"
+    },
+
+    "yogurt di cocco": {
+        color: "green"
+    },
+
+    "cheddar": {
+        color: "green"
+    },
+
+    "feta": {
+        color: "green"
+    },
+
+    "mozzarella": {
+        color: "green"
+    },
+
+    "brie": {
+        color: "green"
+    },
+
+    "camembert": {
+        color: "green"
+    },
+
+    "parmigiano": {
+        color: "green"
+    },
+
+    "gorgonzola": {
+        color: "green"
+    },
+
+    "edam": {
+        color: "green"
+    },
+
+    "formaggio di capra": {
+        color: "green"
+    },
+
+    "ricotta": {
+        color: "green",
+        portion: "max 2 cucchiai"
+    },
+
+    "fiocchi di latte": {
+        color: "green",
+        portion: "max 2 cucchiai"
+    },
+
+    "mascarpone": {
+        color: "green",
+        portion: "max 2 cucchiai"
+    },
+
+    "halloumi": {
+        color: "green",
+        portion: "max 2 fette"
+    },
+
+    "burro": {
+        color: "green"
+    },
+
+    "panna": {
+        color: "green"
+    },
+
+    "panna acida": {
+        color: "green"
+    },
+
+    "crema pasticcera": {
+        color: "red",
+        portion: "OK 2 cucchiai"
+    },
+
+    "gelato": {
+        color: "red",
+        portion: "OK 1 pallina"
+    },
+
+    "gelato senza lattosio": {
+        color: "green"
+    },
+
+    "gelato di soia": {
+        color: "green"
+    },
+
+    "cioccolato al latte": {
+        color: "green",
+        portion: "max 30 g"
+    },
+
+    "cioccolato bianco": {
+        color: "green",
+        portion: "max 30 g"
+    },
+
+    "cioccolato fondente": {
+        color: "green"
+    },
+
+
+    // =========================
+    // CARNE, PESCE E UOVA
+    // =========================
+
+    "manzo": {
+        color: "green"
+    },
+
+    "pollo": {
+        color: "green"
+    },
+
+    "maiale": {
+        color: "green"
+    },
+
+    "pancetta": {
+        color: "green"
+    },
+
+    "prosciutto": {
+        color: "green"
+    },
+
+    "tacchino": {
+        color: "green"
+    },
+
+    "pesce": {
+        color: "green"
+    },
+
+    "frutti di mare": {
+        color: "green"
+    },
+
+    "pesce in scatola": {
+        color: "green"
+    },
+
+    "uova": {
+        color: "green"
+    },
+
+    "tofu": {
+        color: "green"
+    },
+
+
+    // =========================
+    // CONDIMENTI E AROMI
+    // =========================
+
+    "sale": {
+        color: "green"
+    },
+
+    "pepe": {
+        color: "green"
+    },
+
+    "aceto di riso": {
+        color: "green"
+    },
+
+    "aceto bianco": {
+        color: "green"
+    },
+
+    "aceto balsamico": {
+        color: "green",
+        portion: "Massimo: 1 cucchiaio"
+    },
+
+    "erbe aromatiche": {
+        color: "green"
+    },
+
+    "spezie": {
+        color: "green"
+    },
+
+    "cannella": {
+        color: "green"
+    },
+
+    "coriandolo": {
+        color: "green"
+    },
+
+    "zenzero": {
+        color: "green"
+    },
+
+    "succo di limone": {
+        color: "green"
+    },
+
+    "senape": {
+        color: "green"
+    },
+
+
+
+    // =========================
+    // BISCOTTI, CEREALI E SNACK
+    // =========================
+
+    "porridge": {
+        color: "green"
+    },
+
+    "cornflakes": {
+        color: "green"
+    },
+
+    "rice krispies": {
+        color: "green"
+    },
+
+    "pop corn": {
+        color: "green"
+    },
+
+    "patatine": {
+        color: "green"
+    },
+
+    "triangolini di mais": {
+        color: "green"
+    },
+
+    "biscotti": {
+        color: "red"
+    },
+
+    "fette biscottate": {
+        color: "red"
+    },
+
+    "muesli": {
+        color: "red"
+    },
+
+    "weetabix": {
+        color: "red"
+    },
+
+    "bran flakes": {
+        color: "red"
+    },
 
 };
 
@@ -36,51 +888,41 @@ const portion = document.getElementById("portion");
 const notFound = document.getElementById("notFound");
 
 
-search.addEventListener("input", function() {
+search.addEventListener("input", function () {
 
     const query = search.value
         .trim()
         .toLowerCase();
 
-
     if (query === "") {
-
         result.classList.add("hidden");
         notFound.classList.add("hidden");
-
         return;
     }
 
-
-    const food = Object.keys(foods).find(function(name) {
-
+    const food = Object.keys(foods).find(function (name) {
         return name.includes(query);
-
     });
 
-
     if (!food) {
-
         result.classList.add("hidden");
         notFound.classList.remove("hidden");
-
         return;
     }
-
 
     const data = foods[food];
 
-
     foodName.textContent = food;
 
+    circle.className = "circle " + data.color;
 
-    circle.className =
-        "circle " + data.color;
-
-
-    portion.textContent =
-        data.portion;
-
+    if (data.portion) {
+        portion.textContent = data.portion;
+        portion.classList.remove("hidden");
+    } else {
+        portion.textContent = "";
+        portion.classList.add("hidden");
+    }
 
     result.classList.remove("hidden");
     notFound.classList.add("hidden");
